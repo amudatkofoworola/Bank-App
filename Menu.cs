b429469aeb859d07eee5ad92be4315d78df1686d
Kofo Olatunde <amudatkofoworola@yahoomail.com>
2022-03-29 10:52:59 +0100

    added a space

--- Menu.cs
+++ Menu.cs
@@ -29,7 +29,7 @@ namespace ProjectBankApp
             {
                 Console.WriteLine("Invalid input");
             }
-                      
+                                  
             else if(option==1)
             {
                 RegisterCustomerMenu();
